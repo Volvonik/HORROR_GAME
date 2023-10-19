678f82200c9b774a10956a9886c74ad39c18981f
Volvonik <volvo0unity@gmail.com>
2023-10-19 22:02:39 +0300

    OZ

--- HorrorGameJam/Assets/scripts/player/move_water.cs
+++ HorrorGameJam/Assets/scripts/player/move_water.cs
@@ -4,39 +4,73 @@ using UnityEngine;
 
 public class move_water : MonoBehaviour
 {
+
+    Rigidbody2D rb;
+
     [SerializeField]
     float speed;
 
     public static Vector2 force;
 
     public static Vector2 movement;
-    
-    // Start is called before the first frame update
+
+
+    [Header("Flashlight")]
+    bool hasFlashlight;
+    [SerializeField] LayerMask flashlightLayer;
+    [SerializeField] float raycastLength;
+    [SerializeField] GameObject flashlightCheck;
+    [SerializeField] GameObject falshlightText;
+    [SerializeField] Sprite defaultSprite;
+    [SerializeField] Sprite hasFlashlightSprite;
+    [SerializeField] GameObject flashlightLight2D;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         force = new Vector2(1, 1);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-
-
         float MoveHorizontal = Input.GetAxis("Horizontal");
-
         float MoveVertical = Input.GetAxis("Vertical");
 
-
-
         rb.velocity = new Vector2(speed * MoveHorizontal * force.x, MoveVertical * speed * force.y);
+
+        PickFlashLight();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    void PickFlashLight()
     {
-        if(collision.gameObject.CompareTag("FlashLight"))
+        RaycastHit2D flashlightRaycast = Physics2D.Raycast(flashlightCheck.transform.position, -transform.right, raycastLength, flashlightLayer);
+        Debug.DrawRay(flashlightCheck.transform.position, -transform.right * raycastLength, Color.green);
+
+        if (flashlightRaycast)
         {
-            print("got flashlight!");
+            falshlightText.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                hasFlashlight = true;
+
+                Destroy(flashlightRaycast.transform.gameObject);
+            }
         }
+        else
+        {
+            falshlightText.SetActive(false);
+        }
+
+        flashlightLight2D.SetActive(hasFlashlight);
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (hasFlashlight)
+        {
+            spriteRenderer.sprite = hasFlashlightSprite;
+            return;
+        }
+
+        spriteRenderer.sprite = defaultSprite;
     }
 }
