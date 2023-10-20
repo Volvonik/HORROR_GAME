b4b9b0a7da488c854db2be2cbb2e0a7933e8c50d
Volvonik <volvo0unity@gmail.com>
2023-10-20 10:03:00 +0300

    tvlo

--- HorrorGameJam/Assets/scripts/PanasEManager.cs
+++ HorrorGameJam/Assets/scripts/PanasEManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PanasEManager : MonoBehaviour
 {
@@ -30,8 +31,8 @@ public class PanasEManager : MonoBehaviour
     {
         random = Random.Range(spawnDelays[0], spawnDelays[1]);
 
-        float currentYPosition = Random.Range(yPositionBorders[0], yPositionBorders[1]);
-        Vector3 prefabPosition = new Vector3(xPosition, currentYPosition, 0f);
+        float currentYPosition = FindObjectOfType<move_water>().transform.position.y + Random.Range(yPositionBorders[0], yPositionBorders[1]);
+        Vector3 prefabPosition = new Vector3(FindObjectOfType<move_water>().transform.position.x + xPosition, currentYPosition, 0f);
         GameObject prefab = Instantiate(panasE, prefabPosition, Quaternion.identity);
 
         timer = 0;
--- HorrorGameJam/Assets/scripts/Panasye.cs
+++ HorrorGameJam/Assets/scripts/Panasye.cs
@@ -10,8 +10,6 @@ public class Panasye : MonoBehaviour
     {
         transform.Translate(-transform.right * moveSpeed);
 
-        print(transform.position.x - FindObjectOfType<move_water>().gameObject.transform.position.x);
-
         if(Mathf.Abs(transform.position.x - FindObjectOfType<move_water>().gameObject.transform.position.x) > 20 && transform.position.x < FindObjectOfType<move_water>().gameObject.transform.position.x)
         {
             Destroy(gameObject);
