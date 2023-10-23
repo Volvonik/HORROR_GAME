4783ec4624315148a408c8084c177c3bd099556e
Volvonik <volvo0unity@gmail.com>
2023-10-23 20:23:27 +0300

    Fishes

--- HorrorGameJam/Assets/scripts/fish.cs
+++ HorrorGameJam/Assets/scripts/fish.cs
@@ -7,19 +7,13 @@ public class fish : MonoBehaviour
     float timer;
     [SerializeField]
     float speed;
-    int num = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    float num = 0;
 
-    // Update is called once per frame
     void Update()
     {
         if(num == 0)
         {
-            num = Random.Range(3, 8);
+            num = Random.Range(1.5f, 3);
         }
         timer += Time.deltaTime;
         if (timer > num)
@@ -33,11 +27,11 @@ public class fish : MonoBehaviour
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         if(timer < num / 2)
         {
-            rb.velocity = new Vector2(transform.position.x, -speed);
+            rb.velocity = new Vector2(0, -speed);
         }
         if (timer > num / 2)
         {
-            rb.velocity = new Vector2(transform.position.x, speed);
+            rb.velocity = new Vector2(0, speed);
         }
     }
 }
