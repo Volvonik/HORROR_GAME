f0033e0e038edd93a0c0c783a0fc1a48222e1211
amir-m-shulman <amir.shulman08@gmail.com>
2023-10-25 17:02:55 +0300

    bioov script

---
+++ HorrorGameJam/Assets/scripts/END.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class END : MonoBehaviour
+{
+    [SerializeField] GameObject transition;
+    [SerializeField] GameObject story1;
+    [SerializeField] GameObject story2;
+    private bool story;
+    private bool storyt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(story)
+        {
+            story1.SetActive(true);
+            storyt = false;
+            if(Input.GetButtonDown("space"))
+            {
+                story2.SetActive(true);
+                Invoke("Story", 0.2f);
+                if(storyt && Input.GetButtonDown("space"))
+                {
+                    SceneManager.LoadScene(0);
+                }
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Finish"))
+        {
+            transition.SetActive(true);
+            Invoke("Story", 2.3f);
+        }
+    }
+    void Story()
+    {
+        story = true;
+        storyt = true;
+    }
+}
