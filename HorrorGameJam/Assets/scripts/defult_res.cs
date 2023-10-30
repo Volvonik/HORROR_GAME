a6eb15bec830a22a85d47d7c3c3b4e3c6096d00e
amir-m-shulman <amir.shulman08@gmail.com>
2023-10-30 20:43:55 +0200

    set defult resolution

---
+++ HorrorGameJam/Assets/scripts/defult_res.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class defult_res : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
+
+    // Update is called once per frame
+    
+}
