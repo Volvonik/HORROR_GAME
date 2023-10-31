e54f2f5ca330dff3fc5c113e185330eaf4538d55
amir-m-shulman <amir.shulman08@gmail.com>
2023-10-31 19:45:23 +0200

    fix resolution changing on return

--- HorrorGameJam/Assets/scripts/defult_res.cs
+++ HorrorGameJam/Assets/scripts/defult_res.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class defult_res : MonoBehaviour
 {
+    private static bool once;
     // Start is called before the first frame update
     void Start()
     {
-        Screen.SetResolution(1920, 1080, true);
+        if (!once) { Screen.SetResolution(1920, 1080, true); once = true; }
+      
     }
 
     // Update is called once per frame
