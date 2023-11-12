a0dd61a70cbc62bc816d8bdf9882f3b0c6ba5b83
amir-m-shulman <amir.shulman08@gmail.com>
2023-11-12 19:07:05 +0200

    optinize game

--- HorrorGameJam/Assets/scripts/checkpoint.cs
+++ HorrorGameJam/Assets/scripts/checkpoint.cs
@@ -7,15 +7,23 @@ public class checkpoint : MonoBehaviour
     public static Vector2 position;
     public static bool didsave;
     public AudioClip musicAfterCheckpoint;
-    
+    Animator animator;
+    AudioSource ao;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        ao = GetComponent<AudioSource>();
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             didsave = true;
             position = transform.position;
-            GetComponent<Animator>().SetTrigger("checkpoint");
-            GetComponent<AudioSource>().Play();
+            animator.SetTrigger("checkpoint");
+            ao.Play();
             move_water.lastCheckpointMusic = musicAfterCheckpoint;
         }
     }
