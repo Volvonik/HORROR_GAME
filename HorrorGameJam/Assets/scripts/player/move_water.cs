c341b67dcdf3f0b33a57c5c1cd1f6006417c9937
Volvonik <volvo0unity@gmail.com>
2023-11-29 16:54:57 +0200

    all bugs fixed!

--- HorrorGameJam/Assets/scripts/player/move_water.cs
+++ HorrorGameJam/Assets/scripts/player/move_water.cs
@@ -103,7 +103,10 @@ public class move_water : MonoBehaviour
         //hasFlashlight = true;
         //hasFlashlight = false;
 
-
+        if(hasFlashlight)
+        {
+            flashlightLight2D.SetActive(false);
+        }
 
         //checkpoint.didsave = false;
         if (checkpoint.didsave)
